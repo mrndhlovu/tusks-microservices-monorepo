502866b3d2f9e7fbec281911e7a0b424a7287fa4
mndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2022-08-24 23:47:00 +0100

    setup docker files

--- libs/api/shared-middleware/src/lib/auth.ts
+++ libs/api/shared-middleware/src/lib/auth.ts
@@ -1,27 +1,27 @@
-import { Response, NextFunction, Request } from 'express';
-import { validationResult } from 'express-validator';
-import * as jwt from 'jsonwebtoken';
-import { IJwtAccessTokens, IJwtAuthToken } from '@tusks/api/util-interfaces';
+import { Response, NextFunction, Request } from 'express'
+import { validationResult } from 'express-validator'
+import * as jwt from 'jsonwebtoken'
+import { IJwtAccessTokens, IJwtAuthToken } from '@tusks/api/util-interfaces'
 import {
   errorService,
   NotAuthorisedError,
   RequestValidationError,
-} from '@tusks/api/shared-services';
+} from '@tusks/api/shared-services'
 
-const { catchAsyncError } = errorService;
+const { catchAsyncError } = errorService
 
 declare global {
   namespace Express {
     interface Request {
-      currentUserJwt: IJwtAuthToken;
-      apolloAuthToken: string;
+      currentUserJwt: IJwtAuthToken
+      apolloAuthToken: string
       session?: {
-        jwt: IJwtAccessTokens;
-        httpOnly?: boolean;
-        domain?: string;
-        sameSite?: boolean;
-        signed?: boolean;
-      } | null;
+        jwt: IJwtAccessTokens
+        httpOnly?: boolean
+        domain?: string
+        sameSite?: boolean
+        signed?: boolean
+      } | null
     }
   }
 }
@@ -29,72 +29,72 @@ declare global {
 class AuthMiddleWare {
   checkIsAuthenticated = catchAsyncError(
     async (req: Request, res: Response, next: NextFunction) => {
-      const authorization = req?.get('Authorization')!?.replace('Bearer ', '');
+      const authorization = req?.get('Authorization')!?.replace('Bearer ', '')
       const authorizationToken =
-        authorization !== '' ? authorization : undefined;
+        authorization !== '' ? authorization : undefined
 
       if (req?.currentUserJwt?.userId) {
-        next();
+        next()
       }
       const sessionJwtToken =
         req.session && req?.session!.jwt?.access
           ? req?.session!.jwt?.access
-          : authorizationToken;
+          : authorizationToken
 
       if (!sessionJwtToken) {
-        req.session = null;
-        throw new NotAuthorisedError('Authorization credentials are missing.');
+        req.session = null
+        throw new NotAuthorisedError('Authorization credentials are missing.')
       }
 
       const currentUserJwt = jwt.verify(
         sessionJwtToken,
         process.env.JWT_ACCESS_TOKEN_SIGNATURE!
-      );
+      )
 
-      req.currentUserJwt = currentUserJwt as IJwtAuthToken;
+      req.currentUserJwt = currentUserJwt as IJwtAuthToken
 
-      next();
+      next()
     }
-  );
+  )
 
   validateRequestBodyFields = catchAsyncError(
     async (req: Request, _res: Response, next: NextFunction) => {
-      const errors = validationResult(req);
+      const errors = validationResult(req)
 
       if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
+        throw new RequestValidationError(errors.array())
       }
 
-      next();
+      next()
     }
-  );
+  )
 
   validateRequiredAccessJwt = catchAsyncError(
     async (req: Request, _res: Response, next: NextFunction) => {
       if (!req.session || !req.session.jwt) {
-        throw new NotAuthorisedError('Authorization credentials are missing.');
+        throw new NotAuthorisedError('Authorization credentials are missing.')
       }
 
-      next();
+      next()
     }
-  );
+  )
 
   validateRequiredRefreshJwt = catchAsyncError(
     async (req: Request, _res: Response, next: NextFunction) => {
       if (!req.session || !req.session.jwt?.refresh) {
-        throw new NotAuthorisedError('Authorization credentials are missing.');
+        throw new NotAuthorisedError('Authorization credentials are missing.')
       }
 
       const currentUserJwt = jwt.verify(
         req.session.jwt.refresh,
         process.env.JWT_REFRESH_TOKEN_SIGNATURE!
-      );
+      )
 
-      req.currentUserJwt = currentUserJwt as IJwtAuthToken;
+      req.currentUserJwt = currentUserJwt as IJwtAuthToken
 
-      next();
+      next()
     }
-  );
+  )
 }
 
-export const authMiddleware = new AuthMiddleWare();
+export const authMiddleware = new AuthMiddleWare()
